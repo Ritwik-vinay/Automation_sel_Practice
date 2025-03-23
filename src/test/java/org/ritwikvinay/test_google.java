73bc3f71f2ff64d0183418123e7f7d3729a0c3ed
ritwik vinay <ritwikvinay38@gmail.com>
2025-03-23 13:26:06 +0530

    Commit 1

--- src/test/java/org/ritwikvinay/test_google.java
+++ src/test/java/org/ritwikvinay/test_google.java
@@ -1,4 +1,41 @@
 package org.ritwikvinay;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.security.Key;
+
 public class test_google {
+    @Test
+    public void get_web() throws InterruptedException {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-blink-features=AutomationControlled");  // Makes Selenium undetectable
+        options.addArguments("--disable-popup-blocking"); // Prevents popups
+        options.addArguments("--disable-notifications");  // Disables any notifications
+
+        // Set up WebDriver
+        WebDriver driver = new ChromeDriver(options);
+
+        // Open Google
+        driver.get("https://www.google.co.in");
+
+        // Locate and click on the search box
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("ATB");
+        searchBox.click();
+        searchBox.sendKeys(Keys.ENTER);
+        Thread.sleep(5000);
+
+        // Close browser
+        driver.quit();
+    }
 }
+
